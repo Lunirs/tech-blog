528c38a293474ed77d1d9db7fff76193ae40d511
Lunirs <dhong0925@gmail.com>
2022-06-21 18:22:18 -0400

    created blogpost model

---
+++ models/Blogpost.js
@@ -0,0 +1,40 @@
+const sequelize = require("../config/connection");
+const { Model, DataTypes } = require("sequelize");
+
+
+class Blogpost extends Model {}
+
+Blogpost.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true
+    },
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    body: {
+      type: DataTypes.STRING,
+      allowNull:false
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "user",
+        key: "id"
+      }
+    }
+  },
+  {
+    sequelize,
+    timestamps: true,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "blogpost",
+  }
+);
+
+module.exports = Blogpost;
