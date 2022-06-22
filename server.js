50cc187c5659c2f26e933a09175596ddfdeb29a6
Lunirs <dhong0925@gmail.com>
2022-06-22 00:04:30 -0400

    installed more npm packages and renamed files

--- server.js
+++ server.js
@@ -4,51 +4,95 @@ const path = require("path");
 const exphbs = require("express-handlebars");
 const helpers = require("./utils/helpers");
 const { User, Blogpost, Comment } = require("./models");
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 const hbs = exphbs.create({ helpers });
 
+const sess = {
+  secret: "Super secret secret",
+  cookie: {
+    maxAge: 36000
+  },
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+      db: sequelize
+  })
+};
+app.use(session(sess));
+
 app.engine("handlebars", hbs.engine);
 app.set("view engine", "handlebars");
 
-app.get("/", async (req,res) => {
+//Home routes
+app.get("/", async (req, res) => {
   try {
-  const blogpostData = await Blogpost.findAll({
-    include: [{
-        model: User,
-        attributes: {exclude: ['password']}
-    }],
-  });
-  
-  const blogposts = blogpostData.map((blogpost) => blogpost.get({plain: true}));
-  
-  res.render("homepage", {blogposts});
-  } catch(err) {
+    const blogpostData = await Blogpost.findAll({
+      include: [
+        {
+          model: User,
+          attributes: { exclude: ["password"] },
+        },
+      ],
+    });
+
+    const blogposts = blogpostData.map((blogpost) =>
+      blogpost.get({ plain: true })
+    );
+
+    res.render("allBlogPost", { blogposts });
+  } catch (err) {
     res.status(500).json(err);
   }
 });
 
-app.get("/dashboard", async (req,res) => {
-    try {
+app.get("/blogpost/:id", async (req, res) => {
+  try {
+    const blogpostData = await Blogpost.findByPk(req.params.id, {
+      include: [
+        User,
+        {
+          model: Comment,
+          include: [{
+            model: User,
+            attributes: {exclude:["password"]};
+          }],
+        },
+      ],
+    });
+
+    const singleBlogPost = blogpostData.get({ plain: true });
+
+    res.render("singleBlogPost", { singleBlogPost });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// dashbaord routes
+app.get("/dashboard", async (req, res) => {
+  try {
     const blogpostData = await Blogpost.findAll({
-      include: [{
-          model: User,
-          attributes: {exclude: ['password']}
-      }],
+      where: {
+        user_id: req.session.user_id
+      }
     });
-    
-    const blogpostdash = blogpostData.map((blogpost) => blogpost.get({plain: true}));
-    
+
+    const blogpostdash = blogpostData.map((blogpost) =>
+      blogpost.get({ plain: true })
+    );
+
     res.render("dashboardpage", {
-        layout: "dashboard",
-        blogpostdash
+      layout: "dashboard",
+      blogpostdash,
     });
-    } catch(err) {
-      res.status(500).json(err);
-    }
-  });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log("Now listening"));
