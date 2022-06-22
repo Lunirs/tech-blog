0103f1305bbcc90618bbe96b640317906691ea58
Lunirs <dhong0925@gmail.com>
2022-06-22 16:38:35 -0400

    Added api route index and updated server so it can use it

--- controllers/index.js
+++ controllers/index.js
@@ -2,8 +2,10 @@ const router = require("express").Router();
 
 const homepageRoutes = require("./homepageRoutes");
 const dashboardRoutes = require("./dashboardRoutes");
+const apiRoutes = require("./api");
 
 router.use("/", homepageRoutes);
 router.use("/dashboard", dashboardRoutes);
+router.use("/api", apiRoutes);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const path = require("path");
 const session = require("express-session");
 const exphbs = require("express-handlebars");
 const helpers = require("./utils/helpers");
+const routes = require("./controllers");
 const SequelizeStore = require("connect-session-sequelize")(session.Store);
 
 const app = express();
@@ -22,12 +23,17 @@ const sess = {
     db: sequelize,
   }),
 };
+
 app.use(session(sess));
-app.use(require("./controllers"));
-app.use(express.static(path.join(__dirname, "public")));
 
 app.engine("handlebars", hbs.engine);
 app.set("view engine", "handlebars");
+
+app.use(express.json());
+app.use(require("./controllers"));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log("Now listening"));
