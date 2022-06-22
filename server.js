f91a34737983fe76eee0fc25e70bdb4dde536b40
Lunirs <dhong0925@gmail.com>
2022-06-21 22:41:41 -0400

    Edited handlebars format

--- server.js
+++ server.js
@@ -30,6 +30,25 @@ app.get("/", async (req,res) => {
   }
 });
 
+app.get("/dashboard", async (req,res) => {
+    try {
+    const blogpostData = await Blogpost.findAll({
+      include: [{
+          model: User,
+          attributes: {exclude: ['password']}
+      }],
+    });
+    
+    const blogpostdash = blogpostData.map((blogpost) => blogpost.get({plain: true}));
+    
+    res.render("dashboardpage", {
+        layout: "dashboard",
+        blogpostdash
+    });
+    } catch(err) {
+      res.status(500).json(err);
+    }
+  });
 
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log("Now listening"));
