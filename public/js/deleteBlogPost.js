1cd140360731dc060383f501b1ecab80604ba722
Lunirs <dhong0925@gmail.com>
2022-06-23 00:35:46 -0400

    Added function to delete post

--- public/js/deleteBlogPost.js
+++ public/js/deleteBlogPost.js
@@ -1,7 +1,21 @@
 const deleteBlogPost = async (event) => {
   event.preventDefault();
 
-  const title = document.querySelector("").value;
-  const body = document.querySelector("").value;
-  const blogpost_id = document.querySelector("");
+  const id = document.querySelector("#blogpost-id").value;
+
+  const response = await fetch(`/api/blogposts/${id}`, {
+    method: "DELETE",
+    body: JSON.stringify({
+      id,
+    }),
+    headers: { "Content-Type": "application/json" },
+  });
+
+  if (response.ok) {
+    document.location.replace("/dashboard");
+  } else {
+    alert("Failed to delete blog post.");
+  }
 };
+
+document.querySelector(".delBtn").addEventListener("click", deleteBlogPost);
