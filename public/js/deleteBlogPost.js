e3b08d2193231cdb280ee6705c826a0b691f3357
Lunirs <dhong0925@gmail.com>
2022-06-23 00:32:48 -0400

    Added edit post feature

---
+++ public/js/deleteBlogPost.js
@@ -0,0 +1,7 @@
+const deleteBlogPost = async (event) => {
+  event.preventDefault();
+
+  const title = document.querySelector("").value;
+  const body = document.querySelector("").value;
+  const blogpost_id = document.querySelector("");
+};
